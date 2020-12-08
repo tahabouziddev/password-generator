cf9847e0932171b22d226521f3b886b746045141
taha <taha.lelouch.bouzid@gmail.com>
2020-12-08 14:26:57 -0800

    storring pwd feature done

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
+
 var array =[
-    "a","b","c","A","Z","E","R","T","Y","U","I","O","P","Q","S","D","F","G","H","J","K","L","M","W","X","C","V","B","N","d","e","f","g","h","j","k","l","m","n","o","p","q","r","t","s","u","v","w","x","y","z","i","1","2","3","4","5","6","7","8","9","0","&","-","_","@","#"
+    "a","b","c","A","Z","E","R","T","Y","U","I","O","P","Q","S","D","F","G","H","J","K","L","M","W","X","C","V","B","N","d","e","f","g","h","j","k","l","m","n","o","p","q","r","t","s","u","v","w","x","y","z","i","1","2","3","4","5","6","7","8","9","0","-","_","@","#"
 ];
 
 function myfunction() {
-    var x = array[ Math.floor(Math.random() * 66)]
-var c = array[ Math.floor(Math.random() * 66)]
-var a = array[ Math.floor(Math.random() * 66)]
-var z = array[ Math.floor(Math.random() * 66)]
-var e = array[ Math.floor(Math.random() * 66)]
-var r = array[ Math.floor(Math.random() * 66)]
-var t = array[ Math.floor(Math.random() * 66)]
-var y = array[ Math.floor(Math.random() * 66)]
-var u = array[ Math.floor(Math.random() * 66)]
-var i = array[ Math.floor(Math.random() * 66)]
-var o = array[ Math.floor(Math.random() * 66)]
-var p = array[ Math.floor(Math.random() * 66)]
-var q = array[ Math.floor(Math.random() * 66)]
-var s = array[ Math.floor(Math.random() * 66)]
-var d = array[ Math.floor(Math.random() * 66)]
-var f = array[ Math.floor(Math.random() * 66)]
-var h = array[ Math.floor(Math.random() * 66)]
-var j = array[ Math.floor(Math.random() * 66)]
-var k = array[ Math.floor(Math.random() * 66)]
-var l = array[ Math.floor(Math.random() * 66)]
-var m = array[ Math.floor(Math.random() * 66)]
-var w = array[ Math.floor(Math.random() * 66)]
+
+
+
+    var x = array[ Math.floor(Math.random() * 67)]
+var c = array[ Math.floor(Math.random() * 67)]
+var a = array[ Math.floor(Math.random() * 67)]
+var z = array[ Math.floor(Math.random() * 67)]
+var e = array[ Math.floor(Math.random() * 67)]
+var r = array[ Math.floor(Math.random() * 67)]
+var t = array[ Math.floor(Math.random() * 67)]
+var y = array[ Math.floor(Math.random() * 67)]
+var u = array[ Math.floor(Math.random() * 67)]
+var i = array[ Math.floor(Math.random() * 67)]
+var o = array[ Math.floor(Math.random() * 67)]
+var p = array[ Math.floor(Math.random() * 67)]
+var q = array[ Math.floor(Math.random() * 67)]
+var s = array[ Math.floor(Math.random() * 67)]
+var d = array[ Math.floor(Math.random() * 67)]
+var f = array[ Math.floor(Math.random() * 67)]
+var h = array[ Math.floor(Math.random() * 67)]
+var j = array[ Math.floor(Math.random() * 67)]
+var k = array[ Math.floor(Math.random() * 67)]
+var l = array[ Math.floor(Math.random() * 67)]
+var m = array[ Math.floor(Math.random() * 67)]
+var w = array[ Math.floor(Math.random() * 67)]
 var password = a+z+e+r+t+y+u+i+o+p+q+s+d+f+h+j+k+l+m+w+x+c
 
     document.getElementById("pwd").value = password;
-  }
+    var webname = document.getElementById("webname").value ;
+    var weburl = document.getElementById("weburl").value ;
+    var username = document.getElementById("username").value ;
 
+
+  $.ajax(
+    {
+        type:'POST',
+        dataType:'json',
+        contentType:'application/json;charset-utf-08',
+        url:'http://127.0.0.1:5000/store?pwd='+ password+"&webname="+ webname+"&weburl="+weburl+"&username="+username,
+        success:function (data) {
+            var reply=data.reply;
+            if (reply=="success")
+            {
+                console.log("st9wd");
+            }
+            else
+                {
+                alert("some error ocured in session agent")
+                }
+
+        }
+    }
+);} 
--- index.py
+++ index.py
@@ -1,4 +1,14 @@
+
+
+
 import mysql.connector
+import flask
+from flask import Flask
+from flask import request
+from flask import jsonify
+app = Flask(__name__)
+
+
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -10,9 +20,22 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 # mycursor.execute("CREATE TABLE passwords (id INT AUTO_INCREMENT PRIMARY KEY, websitename VARCHAR(255), websiteurl VARCHAR(255) , username VARCHAR(255) , password VARCHAR(255))") 
-sql = "INSERT INTO passwords (websitename, websiteurl,username,password) VALUES (%s, %s,%s,%s)"
-val = ("John", "Highway 21","test","test")
-mycursor.execute(sql, val)
+# print request.POST['']  for POST form method
 
-mydb.commit()
+
 print(mycursor.rowcount, "record inserted.")
+
+@app.route('/store',methods=['POST','GET'])
+def pass_val():
+      pwd= request.args.get('pwd')
+      weburl= request.args.get('weburl')
+      webname= request.args.get('webname')
+      username= request.args.get('username')
+      sql = "INSERT INTO passwords (websitename, websiteurl,username,password) VALUES (%s, %s,%s,%s)"
+      val = (webname, weburl,username,pwd)
+      mycursor.execute(sql, val)
+      mydb.commit()
+      return jsonify({'reply':'success'})
+
+if __name__=='__main__':
+  app.run(debug=True) 
